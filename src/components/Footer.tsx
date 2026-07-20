4bd7cba1c4e5d0c01da9ce8c2a6b75091361d672
Vishnu Saravanan <vishnusaravanan369@gmail.com>
2026-07-20 14:44:18 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FileText, ExternalLink } from 'lucide-react';
 export function Footer() {
   const handleViewResume = () => {
     // Replace this URL with actual Google Drive link
-    window.open('https://drive.google.com/file/u/0/d/1wuXGqvJczGMdmY0Z_WU37DnRNLHVCwWe/view?usp=sharing&pli=1');
+    window.open('https://drive.google.com/file/u/0/d/1wuXGqvJczGMdmY0Z_WU37DnRNLHVCwWe/view');
   };
 
   return (
