1ddea23a038f3fd5d27fc83f05539751acd3dedb
Vishnu Saravanan <vishnusaravanan369@gmail.com>
2026-07-20 14:26:30 +0530

    updated cv

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FileText, ExternalLink } from 'lucide-react';
 export function Footer() {
   const handleViewResume = () => {
     // Replace this URL with actual Google Drive link
-    window.open('https://drive.google.com/file/d/1Wq-qWEo2OCUd0PLiZl333cptVKnyI8zu/view?usp=sharing', '_blank');
+    window.open('https://drive.google.com/file/d/1Wq-qWEo2OCUd0PLiZl333cptVKnyI8zu/view?usp=sharing');
   };
 
   return (
